1f4bf0ca825be81be96587f43600900d280b82ad
vikzh <vikt.anmail@gmail.com>
2023-04-14 22:43:53 +0200

    eslint fixed

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -2,7 +2,6 @@
 import runEngine from '../src/index.js';
 import generateRound from '../src/games/gcd.js';
 
-
 const rules = 'Find the greatest common divisor of given numbers.\n';
 
 runEngine(generateRound, rules);
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
+/* eslint-disable import/prefer-default-export */
 export const getRandomInRange = (max, min) => Math.round(Math.random() * (max - min) + min);
